c604eab87af0ac146b024d355254928f041f5bd0
Ritesh Moharana <riteshmoharana007@gmail.com>
2025-10-11 17:52:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const backendUrl = "https://tm-backend-10.onrender.com/tasks"; // ✅ Your backend base URL
+const backendUrl = "https://tm-backend-10.onrender.com"; // ✅ Your backend base URL
 
 function toggleForms() {
   const loginForm = document.getElementById("loginForm");
@@ -48,5 +48,6 @@ async function login() {
     alert(data.message || "Login failed!");
   }
 }
+
 
 
